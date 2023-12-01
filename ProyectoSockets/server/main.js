3b5adf22e935bf5106e63ed145d24285b98c819a
wodahS21 <yaelelyolo@gmail.com>
2023-12-01 09:59:40 -0600

    activamos el socket

--- ProyectoSockets/server/main.js
+++ ProyectoSockets/server/main.js
@@ -12,6 +12,12 @@ var io = require('socket.io')(server);
 app.get('/',function(req,res){
     res.status(200).send("Hola Mundo");
 });
+/*De esta forma activamos socker para que este escuchando 
+mandamos un mensaje de control por consola para saber que pasa 
+y tenemos que hacer que el mensaje venga del navegador web mediante html y JS*/
+io.on('connection', function (socket){
+    console.log('Alguien se ha conectado con socker');
+});
 server.listen(3005, function(){
     console.log("El servidor esta corriendo en http://localhost:3005");
 
