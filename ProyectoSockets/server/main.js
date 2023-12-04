52c5be6483a71f8f3aebf1a1c458588959ab482d
wodahS21 <yaelelyolo@gmail.com>
2023-12-04 09:15:46 -0600

    usamos middleware para usar elementos estaticos

--- ProyectoSockets/server/main.js
+++ ProyectoSockets/server/main.js
@@ -1,20 +1,13 @@
 var express = require('express');
 var app = express();
-
-/*como trabajamos con sockets, es recomendable usar el modulo HTTP
-   para pasarle la app a express y manejar bien http*/
-   var server = require('http').Server(app);
-/* aqui estara toda la funcionalidad de los sockets
-se requiere la libreria socket.io
-se pasa la variable Server que tiene la app express y HTTP*/   
+var server = require('http').Server(app);
 var io = require('socket.io')(server); 
 
+app.use(express.static('public'));
 app.get('/',function(req,res){
     res.status(200).send("Hola Mundo");
 });
-/*De esta forma activamos socker para que este escuchando 
-mandamos un mensaje de control por consola para saber que pasa 
-y tenemos que hacer que el mensaje venga del navegador web mediante html y JS*/
+
 io.on('connection', function (socket){
     console.log('Alguien se ha conectado con socker');
 });
