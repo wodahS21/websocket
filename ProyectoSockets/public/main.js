ce07fd51912ebbf0701314e8e68532de19715daa
wodahS21 <yaelelyolo@gmail.com>
2023-12-07 09:45:12 -0600

    correccion de errores en los sockets

--- ProyectoSockets/public/main.js
+++ ProyectoSockets/public/main.js
@@ -1,31 +1,30 @@
-const { text } = require("express");
+//const { text } = require("express");
 
-var socket = io.connect('http://localhost:3005', {'forceNew': true});
+var socket = io.connect('http://192.169.1.124:3005', { 'forceNew': true});
 
-/*El cliente maneja datos mediante mensajes, esto se llamara eventos y se mostraran por consola en el navegador*/ 
 socket.on('messages', function(data){
     console.log(data);
     render(data);
 });
-/*Creamos un template para que nos imprima el contenido */
 
 function render(data){
+   
     var html = data.map(function(elem, index){
-        return(`<div> 
-        <strong>${elem.autor}</strong>; 
-        <em>${elem.texto}</em>;
-     </div>`);
-    }).join("");
-     
-    document.getElementById('messages').innerHTML = html;
+        return(`<div>
+                <strong>${elem.autor}</strong>:
+                <em>${elem.texto}</em>
+                </div>`);
+    }).join(" ");
+
+            document.getElementById('messages').innerHTML = html;
 }
 
-// cada ves que alguien precione el boton enviar en el formulario el cliente
-// emite un nuevo mensaje y manda el payload
+
 function addMessage(e){
-var payload ={
-       autor: document.getElementById(username).value,
-       texto: document.getElementById(texto).value
-};
-socket.emit('new-message', payload);
+    var payload = {
+        autor: document.getElementById('username').value,
+        texto: document.getElementById('texto').value
+    };
+    socket.emit('new-message', payload);
+    return false;
 }
--- ProyectoSockets/server/main.js
+++ ProyectoSockets/server/main.js
@@ -1,28 +1,44 @@
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
-var io = require('socket.io')(server); 
-var messages =[ {
-    id:1,
+//var io = require('socket.io')(server);
+var io = require('socket.io')(server, {
+    cors: {
+        origin: '*',
+        methods:['get'],
+        allowedHeaders: ["*"],
+    }
+});
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+var messages = [{
+    id: 1,
     texto: "Hola soy un mensaje",
-    autor: "Yael Avila Melendez"
+    autor: "yael avila melendez"
 }];
-app.use(express.static('public'));
-app.get('/',function(req,res){
-    res.status(200).send("Hola Mundo");
+
+app.use(express.static('../public'));
+
+app.get('/',function(req, res){
+ res.status(200).send("Hola Mundo");
 });
 
-io.on('connection', function (socket){
-    console.log('Alguien se ha conectado con socket');
-    socket.emit('messages',messages);
-    socket.on('new message', function(data){
-    Message.push(data);
+io.on('connection', function(socket){
+    console.log('Alguien se ha conectado con socket')
+    socket.emit('messages', messages);
+    socket.on('new-message', function(data){
+    messages.push(data); 
+    io.sockets.emit('messages', messages);    
+    });
 });
-});
-
-
-
 server.listen(3005, function(){
-    console.log("El servidor esta corriendo en http://localhost:3005");
+    console.log("El servidor esta corriendo en http://192.168.1.124:3005");
 
 });
