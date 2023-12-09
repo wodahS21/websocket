d057f943bf6f8c7e155e8cbc3cf4b988c4c892e3
wodahS21 <yaelelyolo@gmail.com>
2023-12-09 16:31:58 -0600

    cambios en el chat

--- ProyectoSockets/public/main.js
+++ ProyectoSockets/public/main.js
@@ -1,30 +1,38 @@
-//const { text } = require("express");
+//todo Creamos variable que permita la conexion entre back y front 
+var socket = io.connect('http://192.168.0.10:3005', {'forceNew': true });
+//* Esto manda al servidor el mensaje de connect y aparece en consola
 
-var socket = io.connect('http://192.169.1.124:3005', { 'forceNew': true});
+//* El cliente manejara datos mediante mensajes, esto se llamaran evento y se mostraran por consola en el navegador
 
-socket.on('messages', function(data){
+socket.on('messages', function (data) {
     console.log(data);
     render(data);
 });
 
-function render(data){
-   
-    var html = data.map(function(elem, index){
-        return(`<div>
-                <strong>${elem.autor}</strong>:
-                <em>${elem.texto}</em>
-                </div>`);
+function render(data) {
+
+    var html = data.map(function (elem, index) {
+
+        return (`<div>
+                    <strong>${elem.autor}</strong>:
+                    <em>${elem.texto}</em>
+                </div>`);     
     }).join(" ");
 
-            document.getElementById('messages').innerHTML = html;
+    document.getElementById('messages').innerHTML = html;
 }
 
 
-function addMessage(e){
+//* cada ves que alguien presione el boton enviar en el formulario el cliente emite un nuevo mensaje y manda el payload
+function addMessage(e) {
+
     var payload = {
         autor: document.getElementById('username').value,
         texto: document.getElementById('texto').value
     };
+
     socket.emit('new-message', payload);
+    const msj = document.getElementById('text');
+    msj.value='';
     return false;
 }
--- ProyectoSockets/server/main.js
+++ ProyectoSockets/server/main.js
@@ -1,14 +1,20 @@
 var express = require('express');
 var app = express();
+
+/* Aqui estara toda la funcionalidad de los socket, es recomendable usar el moudlo http
+   para pasarle la app a express y manejar bien http */
 var server = require('http').Server(app);
 //var io = require('socket.io')(server);
+
+// SECCION DE CORS
 var io = require('socket.io')(server, {
     cors: {
         origin: '*',
-        methods:['get'],
+        methods: ['get'],
         allowedHeaders: ["*"],
     }
 });
+
 // Configurar cabeceras y cors
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -18,26 +24,37 @@ app.use((req, res, next) => {
     next();
 });
 
+
 var messages = [{
     id: 1,
     texto: "Hola soy un mensaje",
-    autor: "yael avila melendez"
+    autor: "Yael Avila Melendez"
 }];
 
 app.use(express.static('../public'));
 
-app.get('/',function(req, res){
- res.status(200).send("Hola Mundo");
+app.get('/', function(req, res){
+    res.status(200).send("Hola mundo");
 });
 
-io.on('connection', function(socket){
-    console.log('Alguien se ha conectado con socket')
+/* De esta forma activamos socket para que este escuchando mandamos un mensaje de control por consola para saber que pasa y tenemos que hacer que el mensaje venga del navegador web
+   mediante html y JS */
+io.on('connection' , function (socket){
+    console.log('Alguien se ha conectado con socket');
+
+    //Controlador de eventos del cliente mediante sockets
     socket.emit('messages', messages);
+
+    // Ahora queremos escuchar los mensajes mandados por el cliente
     socket.on('new-message', function(data){
-    messages.push(data); 
-    io.sockets.emit('messages', messages);    
+        // Para poder guardar estos mensajes lo ideal seria en una base de datos
+        // Para este ejercicio utilizaremos arrays (esto no es bueno en produccion
+        messages.push(data);
+        //
+        io.sockets.emit('messages', messages);
     });
 });
+
 server.listen(3005, function(){
     console.log("El servidor esta corriendo en http://192.168.1.124:3005");
 
